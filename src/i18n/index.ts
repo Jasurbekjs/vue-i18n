30dc58e1a4f6f19c96d018fda7bc7383f1d1be32
Jasurbek <jasmirzaev@gmail.com>
2023-10-16 20:59:53 +0300

    add i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,37 @@
+import { createI18n } from "vue-i18n";
+
+const messages = {
+    en: {
+        nav: {
+            home: "Home",
+            about: "About"
+        },
+        home: {
+            header: "Welcome to the Vue 3 I18n",
+            added_by: "JMCorp"
+        },
+        about: {
+            header: "About us"
+        }
+    },
+    ru: {
+        nav: {
+            home: "Главная",
+            about: "О нас"
+        },
+        home: {
+            header: "Добро пожаловать во Vue 3 I18n",
+            added_by: "JM Corp"
+        },
+        about: {
+            header: "О нас"
+        }
+    }
+}
+
+export default createI18n({
+    locale: import.meta.env.VITE_DEFAULT_LOCALE,
+    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE, 
+    legacy: false, // for vue 3
+    messages
+})
