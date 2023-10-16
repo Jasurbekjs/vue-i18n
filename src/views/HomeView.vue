ffda83d321c073a2ca9898e3fd9cb766d0a46e39
Jasurbek <jasmirzaev@gmail.com>
2023-10-16 07:03:23 +0300

    default templates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>
+      Welcome to the Vue 3 I18n!
+    </h1>
+    <p>
+      This page has been visited 3 times.
+    </p>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
